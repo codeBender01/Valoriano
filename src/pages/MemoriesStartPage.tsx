29f88021a9c8b94690bb113e20ddb644b337c907
maksat111 <maksatakmyradow111@gmail.com>
2025-05-10 01:05:16 +0500

    Deleted back button

--- src/pages/MemoriesStartPage.tsx
+++ src/pages/MemoriesStartPage.tsx
@@ -4,20 +4,18 @@ import { Button } from "antd";
 
 import { useNavigate } from "react-router-dom";
 
-import { SlArrowLeft } from "react-icons/sl";
-
 const MemoriesStartPage: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative flex items-center justify-center min-h-[100vh] w-[100%]">
-      <div
+    <div className="relative flex items-center justify-center h-[100vh] w-[100%]">
+      {/* <div
         onClick={() => navigate(-1)}
         className={`absolute left-[5%] top-[5%] flex items-center gap-[10px] text-normal cursor-pointer mobile:hidden`}
       >
         <SlArrowLeft />
         Back
-      </div>
+      </div> */}
       <div className="w-[70%] max-w-[300px] sm:max-w-[350px] flex flex-col items-center gap-4">
         <h1 className="font-main font-bold text-[30px] tablet:text-[35px] text-black">
           Voloriano
